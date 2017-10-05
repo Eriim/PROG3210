489b398d557b0a8d605ea2e9d96ffd00764df88f
Eric Tossell <Etossell8259@conestogac.on.ca>
2017-10-05 10:44:39 -0400

    Beginning of create profile page

--- app/src/main/java/com/erictossell/fitnesstracker/createProfile.java
+++ app/src/main/java/com/erictossell/fitnesstracker/createProfile.java
@@ -2,20 +2,79 @@ package com.erictossell.fitnesstracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Interpolator;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class createProfile extends AppCompatActivity {
-    private TextView welcomeTextView;
 
-    private String welcomeText;
+import org.w3c.dom.Text;
+
+public class createProfile extends AppCompatActivity {
+
+    private TextView activityLevelTextView;
+    private TextView caloriesTextView;
+    private EditText nameEditText;
+    private EditText ageEditText;
+    private EditText feetEditText;
+    private EditText inchesEditText;
+    private EditText weightEditText;
+    private EditText goalWeightEditText;
+    private SeekBar activitySeekBar;
+    private Button calculateButton;
+    private RadioButton maleRadioButton;
+    private RadioButton femaleRadioButton;
+
+    private Integer age;
+    private Integer feet;
+    private Integer inches;
+    private Integer weight;
+    private Integer goalWeight;
+    private Integer calories;
+    private String name;
+    private String gender;
+    private String activity;
+
+
+
+    private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
-        welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
-        welcomeText = getIntent().getStringExtra("username");
-        welcomeTextView.setText(welcomeText);
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        email = getIntent().getStringExtra("username");
+        nameEditText.setText(email);
 
+        ageEditText = (EditText) findViewById(R.id.ageEditText);
+        feetEditText = (EditText) findViewById(R.id.feetEditText);
+        inchesEditText = (EditText) findViewById(R.id.inchesEditText);
+        weightEditText = (EditText) findViewById(R.id.weightEditText);
+        goalWeightEditText = (EditText) findViewById(R.id.goalWeightEditText);
+        activitySeekBar = (SeekBar) findViewById(R.id.activitySeekBar);
+        ageEditText = (EditText) findViewById(R.id.ageEditText);
+
+        Button calculateButton = (Button) findViewById(R.id.calculateCaloriesButton);
+        calculateButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                name = nameEditText.getText().toString();
+                age = Integer.parseInt(ageEditText.getText().toString());
+                feet = Integer.parseInt(feetEditText.getText().toString());
+                inches = Integer.parseInt(inchesEditText.getText().toString());
+                weight = Integer.parseInt(weightEditText.getText().toString());
+                goalWeight = Integer.parseInt(goalWeightEditText.getText().toString());
+                activity = activityLevelTextView.getText().toString();
+                calculateCalories(age, feet, inches, weight, goalWeight, activity);
+
+            }
+        });
+
+    }
+    public void calculateCalories(Integer age, Integer feet, Integer inches, Integer weight, Integer goalWeight, String activity){
+        
     }
 }
